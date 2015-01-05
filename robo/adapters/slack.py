1087c91ee7cf65720292a08654c231fe96f9eb65
heavenshell <heavenshell.jp@gmail.com>
2015-01-06 00:56:24 +0900

    Add multi-user chat.

--- robo/adapters/slack.py
+++ robo/adapters/slack.py
@@ -17,18 +17,29 @@ logger = logging.getLogger('robo')
 
 
 class SlackXmpp(ClientXMPP):
-    def __init__(self, jid, password):
+    def __init__(self, jid, password, username, room):
+        """Construct a xmpp client.
+
+        :param jid: JID
+        :param password: Slack password
+        :param username: Bot name
+        :param room: Chat room
+        """
         ClientXMPP.__init__(self, jid, password)
         self.add_event_handler('session_start', self.session_start)
         self.add_event_handler('message', self.receive)
         self.signal = None
+        self.room = room
+        self.nick = username
 
     def session_start(self, event):
         self.send_presence()
         self.get_roster()
+        self.plugin['xep_0045'].joinMUC(self.room, self.nick, wait=False)
 
-    def receive(self, msg):
-        self.signal.send(msg.body, original=msg, source='slack')
+    def muc_message(self, msg):
+        if msg['mucnick'] != self.nick and self.nick in msg['body']:
+            self.signal.send(msg['body'], original=msg, source='slack')
 
 
 class Slack(object):
@@ -42,11 +53,18 @@ class Slack(object):
         logger.debug('jid is `{0}`.'.format(jid))
         logger.debug('room is `{0}`.'.format(room))
 
-        self.xmpp = SlackXmpp(jid, password)
+        self.xmpp = SlackXmpp(jid, password, username, room)
+        self.xmpp.register_plugin('xep_0030')
+        self.xmpp.register_plugin('xep_0045')
         self.xmpp.signal = signal
 
     def say(self, message, **kwargs):
-        message.original.reply(message.body).send()
+        self.xmpp.send_message(mto=message.original['from'].bare,
+                               mbody=message.body, mtype='groupchat')
 
     def run(self):
-        pass
+        if self.xmpp.connect():
+            logging.info('Start xmpp client.')
+            self.xmpp.process(block=True)
+        else:
+            logger.error('Cannot start xmpp client.')
